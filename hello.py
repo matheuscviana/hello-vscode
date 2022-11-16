602301c0f08a7d44cd452116ecbfb80d301a4284
Matheus <matheuscviana@gmail.com>
2022-11-16 11:29:06 -0300

    mudei a entrada da funcao hello

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 class Hello:
   def __str__(self):
     return 'Hello World!'
-  def hello(who):
-    return '\nHello ' + who + '!\n'
+  def hello(fulano):
+    return '\nHello ' + fulano + '!\n'
 
 print( Hello().hello('Matheus') )
