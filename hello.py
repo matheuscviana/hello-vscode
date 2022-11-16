7eedb2143ec1ef6b92221250548a5c9db02bb0b3
Matheus <matheuscviana@gmail.com>
2022-11-16 11:20:31 -0300

    Mexi na função hello

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 class Hello:
   def __str__(self):
     return 'Hello World!'
-  def hello(who=''):
-    return '\nHello %s!\n' % who
+  def hello(who):
+    return '\nHello ' + who + '!\n'
 
 print( Hello().hello('Matheus') )
